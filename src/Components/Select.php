5ace1a0a6fd984c9ea5dff83db14e882dc834396
Dmitry <deathburger777@gmail.com>
2021-02-20 19:13:06 +0200

    scaffold

---
+++ src/Components/Select.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Karvaka\Wired\Select\Components;
+
+use Livewire\Component;
+
+class Select extends Component
+{
+
+}
---
+++ src/WiredSelectServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Karvaka\Wired\Select;
+
+use Illuminate\Support\ServiceProvider;
+
+class WiredSelectServiceProvider extends ServiceProvider
+{
+
+}
